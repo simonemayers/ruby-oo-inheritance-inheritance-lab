f2ccc4e3cebf7b5fe98b3e9a9954ce33d9c8cc94
simonemayers <simone21295@gmail.com>
2020-11-30 19:20:33 -0500

    commit tree a655e3047c188778431d086da663939a0f6659f8

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+class Student < User
+    # @knowledge = []
+    
+    
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(info)
+        @knowledge << info
+    end
+
+    def knowledge
+        @knowledge
+    end
 
 end
